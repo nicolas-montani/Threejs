39a16d799d1313f4362510989badbb2b9fe117cd
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2023-03-03 14:56:15 +0100

    Initial commit

---
+++ 03 - Basic Scene /Exercice/script.js
@@ -0,0 +1,30 @@
+const scene = new THREE.Scene();
+
+// red cube 
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+
+// add the cube to the scene
+
+scene.add(mesh);
+
+//sizes 
+const sizes = {
+    width: 800,
+    height: 600,
+}
+
+//camera 
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas : canvas
+
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
